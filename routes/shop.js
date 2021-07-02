272ea19b9a313ac365850429ef6c223f08212009
ast_dklymchuk <d.klymchuk@astoundcommerce.com>
2021-07-02 16:05:13 +0300

    Working with Dynamic Content & Adding Templating Engines

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
+const admimData = require('./admin');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    const products = admimData.products;
+    res.render('shop', {
+                            prods:products, 
+                            pageTitle: 'Shop',
+                            path: '/'
+                        });
 });
 
 module.exports = router;
